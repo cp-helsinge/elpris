4a4d2682a98933dd6da148e2533c0d956358b0ca
kenneth Haldbæk <kenneth@haldbaek.dk>
2022-11-29 17:46:28 +0100

    Kenneth's relay code

---
+++ esp32_relay/relay.py
@@ -0,0 +1,37 @@
+import machine
+import time
+
+
+class RelayControl(object):
+    def __init__(self):
+        self.relay_pins = [27, 32, 25, 33]
+        self.pin = []
+
+        for pin_num in range(4):
+            self.pin.append(machine.Pin(self.relay_pins[pin_num], machine.Pin.OUT))
+
+    def on(self, relay: int):
+        self.pin[relay].value(0)
+
+    def off(self, relay: int):
+        self.pin[relay].value(1)
+
+    def all_on(self, delay: float = .5):
+        for pn in range(4):
+            self.on(relay=pn)
+            time.sleep(delay)
+
+    def all_off(self, delay: float = .5):
+        for pn in range(4):
+            self.off(relay=pn)
+            time.sleep(delay)
+
+
+if __name__ == '__main__':
+    relay = RelayControl()
+    while True:
+        relay.all_on()
+        time.sleep(2)
+        relay.all_off()
+        time.sleep(2)
+
